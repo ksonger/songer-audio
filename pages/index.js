ecca09e5fd9beb1bd33d651f59c03850210d57e4
ksonger <ken@kensonger.com>
2020-08-18 17:27:16 -0700

    graphql working, dynamodb added

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,50 @@
-import {
-  Form,
-  Select,
-  InputNumber,
-  DatePicker,
-  Switch,
-  Slider,
-  Button,
-} from 'antd'
+import React from 'react'
+import Amplify, {API} from "aws-amplify";
+
+import awsMobile from '../src/aws-exports'
+import classNames from 'classnames'
+import {createStore} from "redux";
+import {configureStore} from "@reduxjs/toolkit";
 
 import styles from './index.module.scss'
+import {getMenuItemList, listMenuItemLists} from "../src/graphql/queries";
+import {graphqlOperation} from "@aws-amplify/api-graphql";
 
-const FormItem = Form.Item
-const Option = Select.Option
+Amplify.configure(awsMobile);
 
-export default function Home() {
-  return (
-    <div style={{ marginTop: 100 }}>
-      <Form layout="horizontal">
-        <FormItem
-          label="Input Number"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-          <InputNumber
-            size="large"
-            min={1}
-            max={10}
-            style={{ width: 100 }}
-            defaultValue={3}
-            name="inputNumber"
-          />
-          <a href="#">Link</a>
-        </FormItem>
+const Home = ({menuitems}) => {
+  console.log(menuitems);
+    return <div className={styles.home} />
+  }
 
-        <FormItem
-          label="Switch"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-          <Switch defaultChecked name="switch" />
-        </FormItem>
+export default Home
 
-        <FormItem
-          label="Slider"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-          <Slider defaultValue={70} />
-        </FormItem>
+export const getServerSideProps = async() => {
+  let result;
 
-        <FormItem
-          label="Select"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-          <Select
-            size="large"
-            defaultValue="lucy"
-            style={{ width: 192 }}
-            name="select"
-          >
-            <Option value="jack">jack</Option>
-            <Option value="lucy">lucy</Option>
-            <Option value="disabled" disabled>
-              disabled
-            </Option>
-            <Option value="yiminghe">yiminghe</Option>
-          </Select>
-        </FormItem>
+  try {
+    result = await API.graphql(graphqlOperation(listMenuItemLists))
+  } catch(err) {
+    console.warn(err)
+    return {
 
-        <FormItem
-          label="DatePicker"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 8 }}
-        >
-          <DatePicker name="startDate" />
-        </FormItem>
-        <FormItem style={{ marginTop: 48 }} wrapperCol={{ span: 8, offset: 8 }}>
-          <Button size="large" type="primary" htmlType="submit">
-            OK
-          </Button>
-          <Button size="large" style={{ marginLeft: 8 }}>
-            Cancel
-          </Button>
-        </FormItem>
-      </Form>
-    </div>
-  )
+        menuitems: []
+
+    }
+  }
+  if(result.errors) {
+    console.warn("Failed to fetch menu items. ", result.errors)
+    return {
+
+        menuitems: []
+
+    }
+  }
+  if(result.data)  {
+    return {
+
+        menuitems: result.data.listMenuItemLists.items
+
+    }
+  }
 }
